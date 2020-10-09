577a61c643b1bc36c1181686e961091717423fae
kkvanonymous <ninja.kunal01@gmail.com>
2020-10-09 17:19:29 +0530

    Update app.js

--- api/app.js
+++ api/app.js
@@ -4,22 +4,22 @@ const bot = new Telegraf(process.env.BOT_TOKEN)
 
 bot.start(ctx=>ctx.reply("Lucy test bot online..."))
 
-const evresp = (gevent) => {
-    switch (gevent) {
+// const evresp = (gevent) => {
+//     switch (gevent) {
 
-        case "issues":
-            return `
-Issue Title and Number  : ${process.env.INPUT_IU_TITLE} | #${process.env.INPUT_IU_NUM}
-Commented or Created By : \`${process.env.INPUT_IU_ACTOR}\`
-Issue Body : *${process.env.INPUT_IU_BODY}*
-[Link to Issue](https://github.com/${process.env.GITHUB_REPOSITORY}/issues/${process.env.INPUT_IU_NUM})
-[Link to Repo ](https://github.com/${process.env.GITHUB_REPOSITORY}/)`
+//         case "issues":
+//             return `
+// Issue Title and Number  : ${process.env.INPUT_IU_TITLE} | #${process.env.INPUT_IU_NUM}
+// Commented or Created By : \`${process.env.INPUT_IU_ACTOR}\`
+// Issue Body : *${process.env.INPUT_IU_BODY}*
+// [Link to Issue](https://github.com/${process.env.GITHUB_REPOSITORY}/issues/${process.env.INPUT_IU_NUM})
+// [Link to Repo ](https://github.com/${process.env.GITHUB_REPOSITORY}/)`
 
-        default:
-            return `
-            Repository:${process.env.GITHUB_REPOSITORY}`
-    }
-}
-const output = evresp(process.env.GITHUB_EVENT_NAME)
-bot.telegram.sendMessage(process.env.MY_ID,output,{parse_mode : "Markdown"})
+//         default:
+//             return `
+//             Repository:${process.env.GITHUB_REPOSITORY}`
+//     }
+// }
+// const output = evresp(process.env.GITHUB_EVENT_NAME)
+// bot.telegram.sendMessage(process.env.MY_ID,output,{parse_mode : "Markdown"})
 bot.launch()
